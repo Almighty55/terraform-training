86757cd573cb31ce96afd69673c75a965f1c490d
Almighy55 <mishuiscool@gmail.com>
2022-03-12 17:14:07 -0600

    feat: :sparkles: automate setup script
    now the setup script can pull the bucket and region from the tf file, less places to declare your bucket name upon initial setup

--- aws/scripts/tf_destroy.ps1
+++ aws/scripts/tf_destroy.ps1
@@ -13,18 +13,18 @@ else {
 Set-Location -Path ..
 # auto approve destroy and hide output since it would error out for saving state file
 terraform destroy --auto-approve >$null 2>&1
-if (Test-Path ".terraform" ) {
+if (Test-Path ".terraform") {
     Remove-Item -Path ".terraform" -Recurse -Force
 }
-if (Test-Path "errored.tfstate" ) {
+if (Test-Path "errored.tfstate") {
     Remove-Item -Path "errored.tfstate" -Recurse -Force
 }
-if (Test-Path ".terraform.lock.hcl" ) {
+if (Test-Path ".terraform.lock.hcl") {
     Remove-Item -Path ".terraform.lock.hcl" -Recurse -Force
 }
-if (Test-Path "terraform.tfstate" ) {
+if (Test-Path "terraform.tfstate") {
     Remove-Item -Path "terraform.tfstate" -Recurse -Force
 }
+
 Write-Host -BackgroundColor Black -ForegroundColor Yellow "WARNING: Terraform backend has been destroyed.`nPlease run tf_state_setup.ps1 to setup backend again."
-
-
+Set-Location -Path -
--- aws/scripts/tf_state_setup.ps1
+++ aws/scripts/tf_state_setup.ps1
@@ -1,8 +1,16 @@
-# create s3 state bucket via cli
-aws s3api create-bucket --bucket "alaraj-terraform-state1" --region "us-east-1"
-#* if anything other than us-east-1 then the location constraint is needed
-#aws s3api create-bucket --bucket "alaraj-terraform-state1" --region "us-east-2" --create-bucket-configuration LocationConstraint="us-east-2"
-# cd to the root then run terraform init
 Set-Location -Path ..
+# Get the bucket name and region from the version.tf configuration
+$tfBucket = Get-Content -Path "version.tf" | Select-String "bucket" | Select-Object -ExpandProperty line
+$bucketname = ([regex]::match($tfBucket, '(?<=")(?:\\.|[^"\\])*(?=")')).Value
+
+$tfRegion = Get-Content -Path "version.tf" | Select-String "region" | Select-Object -ExpandProperty line
+$region = ([regex]::match($tfRegion, '(?<=")(?:\\.|[^"\\])*(?=")')).Value
+
+# create s3 state bucket via cli
+aws s3api create-bucket --bucket $bucketName --region $region
+#* if anything other than us-east-1 then the location constraint is needed
+#aws s3api create-bucket --bucket "ENTER_NAME" --region "ENTER_REGION" --create-bucket-configuration LocationConstraint="ENTER_REGION"
+
+# cd to the root then run terraform init
 terraform init
 terraform apply --auto-approve
