fc6124bf6a39965aa258fd1b47862d3348cf6c22
Almighy55 <mishuiscool@gmail.com>
2022-03-17 23:25:27 -0500

    feat: :zap: wrapped in a try catch
    added better error handling and now have the option to handle different bucket region configs based on what is set in the backedn off the start

--- aws/scripts/tf_state_setup.ps1
+++ aws/scripts/tf_state_setup.ps1
@@ -1,25 +1,38 @@
-# test aws creds
+
 try {
-    Get-STSCallerIdentity | Out-Null
+    # test aws creds
+    try {
+        Get-STSCallerIdentity | Out-Null
+    }
+    catch {
+        # once selenium script is finished that should be plugged in here to auto update
+        Update-eAWSCreds
+    } 
+
+    Set-Location -Path ..
+    # Get the bucket name and region from the version.tf configuration
+    $tfBucket = Get-Content -Path "version.tf" | Select-String "bucket" | Select-Object -ExpandProperty line
+    $bucketname = ([regex]::match($tfBucket, '(?<=")(?:\\.|[^"\\])*(?=")')).Value
+
+    $tfRegion = Get-Content -Path "version.tf" | Select-String "region" | Select-Object -ExpandProperty line
+    $region = ([regex]::match($tfRegion, '(?<=")(?:\\.|[^"\\])*(?=")')).Value
+
+    # create s3 state bucket via cli
+    if ($region -eq "us-east-1") {
+        aws s3api create-bucket --bucket $bucketName --region $region >$null 2>&1
+    }
+    else {
+        # if anything other than us-east-1 then the location constraint is needed
+        aws s3api create-bucket --bucket $bucketName --region $region --create-bucket-configuration LocationConstraint=$region >$null 2>&1
+    }
+
+    # cd to the root then run terraform init
+    terraform init
+    terraform apply --auto-approve
 }
 catch {
-    # once selenium script is finished that should be plugged in here to auto update
-    Update-eAWSCreds
-} 
-
-Set-Location -Path ..
-# Get the bucket name and region from the version.tf configuration
-$tfBucket = Get-Content -Path "version.tf" | Select-String "bucket" | Select-Object -ExpandProperty line
-$bucketname = ([regex]::match($tfBucket, '(?<=")(?:\\.|[^"\\])*(?=")')).Value
-
-$tfRegion = Get-Content -Path "version.tf" | Select-String "region" | Select-Object -ExpandProperty line
-$region = ([regex]::match($tfRegion, '(?<=")(?:\\.|[^"\\])*(?=")')).Value
-
-# create s3 state bucket via cli
-aws s3api create-bucket --bucket $bucketName --region $region >$null 2>&1
-#* if anything other than us-east-1 then the location constraint is needed
-#aws s3api create-bucket --bucket "ENTER_NAME" --region "ENTER_REGION" --create-bucket-configuration LocationConstraint="ENTER_REGION"
-
-# cd to the root then run terraform init
-terraform init
-terraform apply --auto-approve
+    Write-Error "There was an error with the setup, please make sure the following are valid:
+    1. bucketname
+    2. region
+    3. aws creds"
+}
