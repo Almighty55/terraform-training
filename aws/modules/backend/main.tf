a97aeae99d00d43354e38637fc82e17f0c8c4a65
Almighy55 <mishuiscool@gmail.com>
2022-06-03 22:22:21 -0500

    refactor: :recycle: cleaning up tfsec issues
    applying block public acls and policy in tf even tho this is applied during the setup via cli commands. Just makes the scan of tfsec better

--- aws/modules/backend/main.tf
+++ aws/modules/backend/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "tf_state_bucket" {
+resource "aws_s3_bucket" "tfstate_bucket" {
   bucket = "alaraj-terraform-state"
   #! THIS IS TRUE FOR TESTING
   force_destroy = true
@@ -7,20 +7,26 @@ resource "aws_s3_bucket" "tf_state_bucket" {
     #prevent_destroy = false
   }
 }
-resource "aws_s3_bucket_versioning" "versioning_tf_state" {
-  bucket = aws_s3_bucket.tf_state_bucket.id
+resource "aws_s3_bucket_versioning" "versioning_tfstate" {
+  bucket = aws_s3_bucket.tfstate_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_tfstate" {
-  bucket = aws_s3_bucket.tf_state_bucket.id
+  bucket = aws_s3_bucket.tfstate_bucket.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_tfstate" {
+  bucket = aws_s3_bucket.tfstate_bucket.id
+  block_public_acls   = true
+  block_public_policy = true
 }
 
 #* useful if multiple people are working on the same codebase
